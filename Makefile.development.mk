3479b821080bf754be09f816bb6556eedc431be7
Nikola Prokopić <nikola@prokopic.rs>
2024-01-04 14:03:48 +0100

    Support longer node pool names and allow dashes
    Ported from https://github.com/giantswarm/cluster-aws/pull/429

--- Makefile.development.mk
+++ Makefile.development.mk
@@ -5,10 +5,6 @@
 ensure-schema-gen:
 	@helm schema-gen --help &>/dev/null || helm plugin install https://github.com/mihaisee/helm-schema-gen.git
 
-.PHONY: generate-schema
-generate-schema: ensure-schema-gen ## Generate the values.schema.json file from the values.yaml
-	@cd helm/cluster && helm schema-gen values.yaml > values.schema.json
-
 ##@ Build
 
 .PHONY: template
@@ -18,3 +14,6 @@ template: ## Output the rendered Helm template
 	$(eval HELM_RELEASE_NAME := $(shell yq .global.metadata.name ${CHART_DIR}/${CI_FILE}))
 	@cd ${CHART_DIR} && \
 		helm template -f ${CI_FILE} --debug ${HELM_RELEASE_NAME} .
+
+.PHONY: generate
+generate: normalize-schema validate-schema generate-docs generate-values
