428c7c33d97c4bd499df3c7f0c9572f1e27559e6
Nikola Prokopić <5638639+nprokopic@users.noreply.github.com>
2024-01-31 11:46:10 +0100

    Fix MachineHealthCheck annotation rendering (#68)
    * Enable MachineHealthCheck in test for required values
    
    * Fix MachineHealthCheck annotation rendering issue
    
    * Update CHANGELOG

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- Fix MachineHealthCheck annotation rendering when custom annotations are not set.
+
 ## [0.7.0] - 2024-01-30
 
 ### Changed
--- Makefile.development.mk
+++ Makefile.development.mk
@@ -7,10 +7,16 @@ ensure-schema-gen:
 
 ##@ Build
 
+TEST_CASE ?=
+ifdef TEST_CASE
+CI_FILE = "ci/test-$(TEST_CASE)-values.yaml"
+else
+CI_FILE ?= "ci/ci-values.yaml"
+endif
+
 .PHONY: template
 template: ## Output the rendered Helm template
 	$(eval CHART_DIR := "helm/cluster")
-	$(eval CI_FILE := "ci/ci-values.yaml")
 	$(eval HELM_RELEASE_NAME := $(shell yq .global.metadata.name ${CHART_DIR}/${CI_FILE}))
 	@cd ${CHART_DIR} && \
 		helm template -f ${CI_FILE} --debug ${HELM_RELEASE_NAME} .
