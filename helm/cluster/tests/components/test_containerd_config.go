a4bdc13d260d1faed3ce9d85aab071b6f8e1acd1
Nikola Prokopić <nikola@prokopic.rs>
2024-07-11 13:23:57 +0200

    Refactor file names

--- helm/cluster/tests/components/test_containerd_config.go
+++ helm/cluster/tests/components/test_containerd_config.go
@@ -13,68 +13,37 @@ import (
 )
 
 var (
-	//go:embed testfiles/containerd_expected_default.toml
+	//go:embed files/containerd_expected_default.toml
 	expectedContainerdDefaultConfig string
 
-	//go:embed testfiles/containerd_expected_zot_mc.toml
+	//go:embed files/containerd_expected_zot_mc.toml
 	expectedContainerdZotMcConfig string
 
-	//go:embed testfiles/containerd_expected_zot_local.toml
+	//go:embed files/containerd_expected_zot_only_local.toml
 	expectedContainerdZotLocalConfig string
 )
 
 var _ = Describe("containerd config", func() {
-	var testHelmValues string
+	DescribeTable("rendered config file",
+		func(helmValuesFile, expectedConfig string) {
+			// Render all cluster chart manifests
+			manifests := helm.Template(
+				helm.GetClusterChartDir(),
+				helmValuesFile,
+				"org-giantswarm",
+				"hello",
+				"--set", "providerIntegration.useReleases=false",
+			)
 
-	// cluster chart manifests
-	var renderedContainerdConfig string
+			// Get containerd config
+			containerdConfigQuery := "select(.kind==\"Secret\") | select (.metadata.name | contains(\"containerd\")) | .data[\"config.toml\"] | @base64d"
+			renderedContainerdConfig := yq.Run(manifests, containerdConfigQuery)
+			renderedContainerdConfig = strings.TrimSpace(renderedContainerdConfig)
 
-	JustBeforeEach(func() {
-		// Render all cluster chart manifests
-		manifests := helm.Template(
-			helm.GetClusterChartDir(),
-			testHelmValues,
-			"org-giantswarm",
-			"hello",
-			"--set", "providerIntegration.useReleases=false",
-		)
-
-		// Get containerd config
-		containerdConfigQuery := "select(.kind==\"Secret\") | select (.metadata.name | contains(\"containerd\")) | .data[\"config.toml\"] | @base64d"
-		renderedContainerdConfig = yq.Run(manifests, containerdConfigQuery)
-		renderedContainerdConfig = strings.TrimSpace(renderedContainerdConfig)
-	})
-
-	When("only required and default Helm values are used", func() {
-		BeforeEach(func() {
-			testHelmValues = "test-required-values.yaml"
-		})
-
-		It("renders expected default containerd config", func() {
-			expectedContainerdDefaultConfig = strings.TrimSpace(expectedContainerdDefaultConfig)
-			Expect(renderedContainerdConfig).To(Equal(expectedContainerdDefaultConfig), fmt.Sprintf("expected:\n>>>%s<<<\n, got:\n>>>%s<<<", expectedContainerdDefaultConfig, renderedContainerdConfig))
-		})
-	})
-
-	When("MC zot is enabled", func() {
-		BeforeEach(func() {
-			testHelmValues = "test-zot-mc-values.yaml"
-		})
-
-		It("renders expected containerd config with MC zot endpoints", func() {
-			expectedContainerdZotMcConfig = strings.TrimSpace(expectedContainerdZotMcConfig)
-			Expect(renderedContainerdConfig).To(Equal(expectedContainerdZotMcConfig), fmt.Sprintf("expected:\n>>>%s<<<\n, got:\n>>>%s<<<", expectedContainerdZotMcConfig, renderedContainerdConfig))
-		})
-	})
-
-	When("local zot is enabled", func() {
-		BeforeEach(func() {
-			testHelmValues = "test-zot-local-values.yaml"
-		})
-
-		It("renders expected containerd config with local zot endpoints", func() {
-			expectedContainerdZotLocalConfig = strings.TrimSpace(expectedContainerdZotLocalConfig)
-			Expect(renderedContainerdConfig).To(Equal(expectedContainerdZotLocalConfig), fmt.Sprintf("expected:\n>>>%s<<<\n, got:\n>>>%s<<<", expectedContainerdZotLocalConfig, renderedContainerdConfig))
-		})
-	})
+			expectedConfig = strings.TrimSpace(expectedConfig)
+			Expect(renderedContainerdConfig).To(Equal(expectedConfig), fmt.Sprintf("expected:\n>>>%s<<<\n, got:\n>>>%s<<<", expectedConfig, renderedContainerdConfig))
+		},
+		Entry("when only required and default Helm values are used", "test-required-values.yaml", expectedContainerdDefaultConfig),
+		Entry("when MC Zot is enabled", "test-zot-mc-values.yaml", expectedContainerdZotMcConfig),
+		Entry("when only local zot is enabled", "test-zot-only-local-values.yaml", expectedContainerdZotLocalConfig))
 })
