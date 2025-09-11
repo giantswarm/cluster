d99ae0f84df749104db61698415dc54214915cff
Nick Jüttner <nick@giantswarm.io>
2025-09-11 11:33:30 +0200

    Drop cgroup v1 (#623)
    * Drop cgroup v1
    
    * Drop cgroup v1
    
    * Fix registry tests

--- helm/cluster/tests/components/test_containerd_config.go
+++ helm/cluster/tests/components/test_containerd_config.go
@@ -25,14 +25,11 @@ var (
 	//go:embed files/containerd_expected_zot_mc_and_local.toml
 	expectedContainerdZotBothMcAndLocalConfig string
 
-	//go:embed files/containerd_expected_without_cgroupsv1.toml
-	expectedContainerdWithoutCgroupsv1Config string
+	//go:embed files/containerd_expected.toml
+	expectedContainerdConfig string
 
-	//go:embed files/containerd_nodepool_expected_without_cgroupsv1.toml
-	expectedContainerdNodePoolWithoutCgroupsv1Config string
-
-	//go:embed files/containerd_nodepool_expected_with_cgroupsv1.toml
-	expectedContainerdNodePoolWithCgroupsv1Config string
+	//go:embed files/containerd_nodepool_expected.toml
+	expectedContainerdNodePoolConfig string
 
 	//go:embed files/containerd_expected_mirrors_registry.toml
 	expectedContainerdDifferentMirrorsConfig string
@@ -62,7 +59,6 @@ var _ = Describe("containerd config", func() {
 		Entry("when only MC Zot is enabled", "test-zot-mc-values.yaml", expectedContainerdZotMcOnlyConfig),
 		Entry("when only local Zot is enabled", "test-zot-only-local-values.yaml", expectedContainerdZotLocalOnlyConfig),
 		Entry("when both local Zot and MC Zot are enabled", "test-zot-mc-and-local-values.yaml", expectedContainerdZotBothMcAndLocalConfig),
-		Entry("when node pools use cgroups v1, control plane always uses cgroups v2", "test-cgroupsv1-values.yaml", expectedContainerdWithoutCgroupsv1Config),
 		Entry("when registry templates are used", "test-registry-mirrors-values.yaml", expectedContainerdDifferentMirrorsConfig))
 
 	DescribeTable("rendered node pools config file",
@@ -84,8 +80,6 @@ var _ = Describe("containerd config", func() {
 			expectedConfig = strings.TrimSpace(expectedConfig)
 			Expect(renderedNodepoolContainerdConfig).To(Equal(expectedConfig), fmt.Sprintf("expected:\n>>>%s<<<\n, got:\n>>>%s<<<", expectedConfig, renderedNodepoolContainerdConfig))
 		},
-		Entry("first node pool uses cgroupsv2", "test-cgroupsv1-values.yaml", "def00", expectedContainerdNodePoolWithoutCgroupsv1Config),
-		Entry("second node pool uses cgroupsv2", "test-cgroupsv1-values.yaml", "def01", expectedContainerdNodePoolWithCgroupsv1Config),
-		Entry("third node pool uses cgroupsv2", "test-cgroupsv1-values.yaml", "def02", expectedContainerdNodePoolWithoutCgroupsv1Config))
+		Entry("when only required and default Helm values are used", "test-required-values.yaml", "def00", expectedContainerdNodePoolConfig))
 
 })
